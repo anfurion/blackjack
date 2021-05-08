5dbcc62cd5e1c3a5652d4320237272411658d163
Anton Sinyuk <anton.sinyuk@gmail.com>
2021-05-08 12:48:02 +0300

    BJ

---
+++ card.rb
@@ -0,0 +1,25 @@
+class Card
+
+  attr_accessor :suit, :rank, :show
+  SUITS = ["Hearts", "Spades", "Clubs", "Diamonds"]
+  RANKS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+  def initialize(suit, rank)
+    @show = true
+    if SUITS.include?(suit) && RANKS.include?(rank)
+      @suit = suit
+      @rank = rank
+    else
+      @suit = 'UNKNOWN'
+      @rank = 'UNKNOWN'
+    end
+  end
+
+  def to_s
+    if show
+      "#{rank} of #{suit}."
+    else
+      'Card is face down right now'
+    end
+  end
+end
